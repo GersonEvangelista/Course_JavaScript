0dfe732c3b82c5af1201bff81f93dfd87d191065
GersonProjects <chancasevangelistagerson@gmail.com>
2023-12-22 17:00:55 -0500

    operadoresBasicos, incremento y decremento, code HTML+JS

--- variables_operadores/javaScript.js
+++ variables_operadores/javaScript.js
@@ -1,4 +1,5 @@
 //VARIABLES
+/*
 var $name, $lastname, years;
 
 //INPUTS
@@ -7,6 +8,24 @@ $lastname = prompt("Insert your lastname");
 years = prompt("Insert your age");
 //OUTPUTS
 alert("Your name is "+$name+"\nyour lastname is "+$lastname+"\nYou are "+years+" years old");
+*/
+
+//OPERADORES BÁSICOS
+var $num1, $num2, $result;
+    $num1 = Number(prompt("Número1")); //Number: para indicarle que se trata de numeros
+    $num2 = Number(prompt("Número2")); //sin Number lo toma como cadenas y en vez de sumar los concatena
+    document.write("<p>num1: "+$num1+"<br>num2: "+$num2+"</p>");
+    $result = $num1+$num2;
+
+    //operadores de incremento y decremento 
+    $num1+=$num2;
+    $num2*=3; //num2 = num2*3
+
+    //CODE HTML + JAVASCRIPT
+    document.write("<p>result = <strong>"+$result+"</strong></p>");
+    document.write("<p>num1+num2 = "+$num1+"<br>num2x3 = "+$num2+"</p>");
+
+
 
 
 
