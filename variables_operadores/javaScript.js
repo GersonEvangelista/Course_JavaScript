1558822f71562e4146ffe873efe9037cdcf66660
GersonProjects <chancasevangelistagerson@gmail.com>
2023-12-22 17:15:09 -0500

    arrays y sus metodos

--- variables_operadores/javaScript.js
+++ variables_operadores/javaScript.js
@@ -1,5 +1,5 @@
 //VARIABLES
-/*
+
 var $name, $lastname, years;
 
 //INPUTS
@@ -7,9 +7,9 @@ $name = prompt("Insert your name")
 $lastname = prompt("Insert your lastname");
 years = prompt("Insert your age");
 //OUTPUTS
-alert("Your name is "+$name+"\nyour lastname is "+$lastname+"\nYou are "+years+" years old");
-*/
+//alert("Your name is "+$name+"\nyour lastname is "+$lastname+"\nYou are "+years+" years old");
 
+/*
 //OPERADORES BÁSICOS
 var $num1, $num2, $result;
     $num1 = Number(prompt("Número1")); //Number: para indicarle que se trata de numeros
@@ -24,8 +24,12 @@ var $num1, $num2, $result;
     //CODE HTML + JAVASCRIPT
     document.write("<p>result = <strong>"+$result+"</strong></p>");
     document.write("<p>num1+num2 = "+$num1+"<br>num2x3 = "+$num2+"</p>");
+*/
 
-
-
+//ARRAYS
+//var data = new Array(); //start with data -> new Array("name");
+var data = []; //start with data -> ["name"];
+data.push($name); data.push($lastname,years); //metodos -> .pop, .unshift, .shift
+alert("Your data is: "+data[0]+" "+data[1]+" "+data[2]); 
 
 
