220c888068320c29c25973a52b666aaea4035ada
GersonProjects <chancasevangelistagerson@gmail.com>
2023-12-22 16:19:23 -0500

    variables, inputs y outputs with alert

---
+++ variables_operadores/javaScript.js
@@ -0,0 +1,12 @@
+//VARIABLES
+var $name, $lastname, years;
+
+//INPUTS
+$name = prompt("Insert your name")
+$lastname = prompt("Insert your lastname");
+years = prompt("Insert your age");
+//OUTPUTS
+alert("Your name is "+$name+"\nyour lastname is "+$lastname+"\nYou are "+years+" years old");
+
+
+
