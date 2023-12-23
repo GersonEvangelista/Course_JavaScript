1d61d8ee1af189681c37976b61c55f6c2dc64779
GersonProjects <chancasevangelistagerson@gmail.com>
2023-12-22 20:58:08 -0500

    Matrices con alert y console.log

--- variables_operadores/javaScript.js
+++ variables_operadores/javaScript.js
@@ -17,19 +17,39 @@ var $num1, $num2, $result;
     document.write("<p>num1: "+$num1+"<br>num2: "+$num2+"</p>");
     $result = $num1+$num2;
 
-    //operadores de incremento y decremento 
+//operadores de incremento y decremento 
     $num1+=$num2;
     $num2*=3; //num2 = num2*3
 
-    //CODE HTML + JAVASCRIPT
+//CODE HTML + JAVASCRIPT
     document.write("<p>result = <strong>"+$result+"</strong></p>");
     document.write("<p>num1+num2 = "+$num1+"<br>num2x3 = "+$num2+"</p>");
-*/
 
 //ARRAYS
 //var data = new Array(); //start with data -> new Array("name");
 var data = []; //start with data -> ["name"];
 data.push($name); data.push($lastname,years); //metodos -> .pop, .unshift, .shift
-alert("Your data is: "+data[0]+" "+data[1]+" "+data[2]); 
+//alert("Your data is: "+data[0]+" "+data[1]+" "+data[2]); 
 
+//MATRICES: Es similar a python, dentro de una lista van otras listas
+var data2 = [];
+data2.push("Jose","Rivera",15);
+
+var matriz = [];
+matriz.push(data,data2); //array que contiene arrays
+alert(matriz[1][2]);
+*/
+
+// Crear una matriz de números y mostrar en consola (F12/fn+F12/inspeccionar para ver consola)
+var matrizNumeros = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  // Acceder a un elemento de la matriz
+  var primerElemento = matrizNumeros[0][0];
+  console.log("Primer elemento:", primerElemento);
+  // Modificar un elemento de la matriz
+  matrizNumeros[1][1] = 10;
+  console.log("Matriz modificada:", matrizNumeros);
 
